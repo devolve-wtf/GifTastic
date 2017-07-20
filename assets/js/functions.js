cea3fbcb50c940ab962f006a63ebb1e146e9a893
kenput3r <devolve.wtf@gmail.com>
2017-07-20 16:56:54 -0700

    added events() and displayResults()

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -2,24 +2,39 @@ function Gify() {
 	this.apiKEY = '62b7c8fe352945ab958002bae159837b';
 	this.category = 'banana';
 	this.categories = ['banana', 'pug', 'car', 'cat']
-	this.dataURL = `http://api.giphy.com/v1/gifs/search?q=${this.category}&api_key=${this.apiKEY}&limit=25`;
+	this.dataURL = `http://api.giphy.com/v1/gifs/search?q=${this.category}&api_key=${this.apiKEY}&limit=20`;
 	this.object = {};
 }
 
+Gify.prototype.displayResults = function() {
+	let gify = this;
+	for(object in gify.object.data) {
+		let stillImage = gify.object.data[object].images.original_still.url;
+		let animatedImage = gify.object.data[object].images.original.url;
+		$('#SearchResults').append(
+			`<div class="col-xs-12 col-sm-6 col-md-3">
+				<img class="img-responsive" src="${stillImage}">
+			</div>`
+		);
+	}
+}
+
 Gify.prototype.search = function() {
-	this.dataURL = `http://api.giphy.com/v1/gifs/search?q=${this.category}&api_key=${this.apiKEY}&limit=25`;
+	let gify = this;
+	gify.dataURL = `http://api.giphy.com/v1/gifs/search?q=${gify.category}&api_key=${gify.apiKEY}&limit=20`;
 	$.ajax({
-		url: this.dataURL,
+		url: gify.dataURL,
 		method: 'GET'
 	}).done(function(res) {
-		this.object = res;
-		console.log(this.object);
+		gify.object = res;
+		console.log(gify.object);
+		gify.displayResults();
 	});
 }
 
 Gify.prototype.defaultCategories = function() {
 	for(category in this.categories) {
-		$('#Categories div').append(`<button class="category">${this.categories[category]}</button>`);
+		$('#Categories div').append(`<button class="category btn btn-default">${this.categories[category]}</button>`);
 	}
 }
 
@@ -28,29 +43,29 @@ Gify.prototype.addCategory = function() {
 
 	if(this.categories.indexOf(category) === -1) {
 		this.categories.push(category);
-		$('#Categories div').append(`<button class="category">${category}</button>`);
-		let self = this;
-		$('.category').click(function() {
-			self.category = $(this).text();
-			self.search();
-		});
+		$('#Categories div').append(`<button id="${category}" class="category btn btn-default">${category}</button>`);
 		$('#NewCategory').val('');
 	}else{
 		$('#NewCategory').val('');
 	}
 }
 
+Gify.prototype.events = function() {
+	let gify = this;
+	$(document).on('click', '.category', function() {
+		gify.category = $(this).text();
+		gify.search();
+	});
+
+	$(document).on('click', '#SubmitCategory', function(event) {
+		event.preventDefault();
+		gify.addCategory();
+	});
+}
+
 var gify = new Gify();
 
 $(document).ready(function() {
 	gify.defaultCategories();
-	$('.category').click(function() {
-		gify.search();
-	});
-	$('#SubmitCategory').click(function(event) {
-		event.preventDefault();
-		//console.log('clicked');
-		//gify.search();
-		gify.addCategory();
-	});
+	gify.events();
 });
